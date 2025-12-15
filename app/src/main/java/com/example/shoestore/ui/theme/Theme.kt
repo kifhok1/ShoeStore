74b78a8b606c41b84e8ffe4b95ff348e7571f880
Барышев Иван <kifhok@gmail.com>
2025-12-15 13:39:53 +0300

    Добавление шрифтов и кастомной темы

--- app/src/main/java/com/example/shoestore/ui/theme/Theme.kt
+++ app/src/main/java/com/example/shoestore/ui/theme/Theme.kt
@@ -1,58 +1,146 @@
 package com.example.shoestore.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.shoestore.R
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+
+private val сolorScheme = CustomColorScheme(
+    red = Red,
+    accent = Accent,
+    disable = Disable,
+    subTextLight = SubTextLight,
+    background = Background,
+    block = Block,
+    text = Text,
+    hint = Hint,
+    subTextDark = SubTextDark,
+    black = Black,
+    white = White
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+val Inter = FontFamily(
+    Font(R.font.raleway, FontWeight.Normal),
+    Font(R.font.raleway_bold, FontWeight.Bold),
+    Font(R.font.raleway_medium, FontWeight.Medium),
+    Font(R.font.raleway_semibold, FontWeight.SemiBold)
 )
+
+val LocalCustomColors = staticCompositionLocalOf {
+    CustomColorScheme(
+        red = Red,
+        accent = Accent,
+        disable = Disable,
+        subTextLight = SubTextLight,
+        background = Background,
+        block = Block,
+        text = Text,
+        hint = Hint,
+        subTextDark = SubTextDark,
+        black = Black,
+        white = White
+    )
+}
+val LocalCustomTypography = staticCompositionLocalOf {
+    CustomTypography(
+        HeadingRegular34 = TextStyle(
+            fontSize = 34.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        ),
+        HeadingRegular32 = TextStyle(
+            fontSize = 32.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        ),
+        HeadingBold30 = TextStyle(
+            fontSize = 30.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Bold
+        ),
+        HeadingRegular26 = TextStyle(
+            fontSize = 26.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        ),
+        HeadingSemiBold16 = TextStyle(
+            fontSize = 16.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.SemiBold
+        ),
+        SubtitleRegular16 = TextStyle(
+            fontSize = 16.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        ),
+        BodyRegular24 = TextStyle(
+            fontSize = 24.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        ),
+        BodyRegular20 = TextStyle(
+            fontSize = 20.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        ),
+        BodySemiBold18 = TextStyle(
+            fontSize = 18.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.SemiBold
+        ),
+        BodyMedium16 = TextStyle(
+            fontSize = 16.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Medium
+        ),
+        BodyRegular16 = TextStyle(
+            fontSize = 16.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        ),
+        BodyMedium14 = TextStyle(
+            fontSize = 14.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Medium
+        ),
+        BodyRegular14 = TextStyle(
+            fontSize = 14.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        ),
+        BodyRegular12 = TextStyle(
+            fontSize = 12.sp,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal
+        )
+    )
+}
 
 @Composable
-fun ShoeStoreTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+fun ShoeStoreTheme(content: @Composable () -> Unit) {
+    val customColors = сolorScheme
+    val customTypography = customTypography
+    CompositionLocalProvider(
+        LocalCustomColors provides customColors,
+        LocalCustomTypography provides customTypography,
         content = content
     )
+}
+
+object CustomTheme {
+    val colors: CustomColorScheme
+        @Composable
+        get() = LocalCustomColors.current
+    val typography: CustomTypography
+        @Composable
+        get() = LocalCustomTypography.current
+
 }
--- app/src/main/java/com/example/shoestore/ui/theme/Type.kt
+++ app/src/main/java/com/example/shoestore/ui/theme/Type.kt
@@ -1,34 +1,101 @@
 package com.example.shoestore.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+@Immutable
+data class CustomTypography(
+    val HeadingRegular34: TextStyle,
+    val HeadingRegular32: TextStyle,
+    val HeadingBold30: TextStyle,
+    val HeadingRegular26: TextStyle,
+    val HeadingSemiBold16: TextStyle,
+    val SubtitleRegular16: TextStyle,
+    val BodyRegular24: TextStyle,
+    val BodyRegular20: TextStyle,
+    val BodySemiBold18: TextStyle,
+    val BodyMedium16: TextStyle,
+    val BodyRegular16: TextStyle,
+    val BodyMedium14: TextStyle,
+    val BodyRegular14: TextStyle,
+    val BodyRegular12: TextStyle
+)
+
+
+val customTypography = CustomTypography(
+    HeadingRegular34 = TextStyle(
+        fontSize = 34.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    HeadingRegular32 = TextStyle(
+        fontSize = 32.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
+    ),
+    HeadingBold30 = TextStyle(
+        fontSize = 30.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Bold
+    ),
+    HeadingRegular26 = TextStyle(
+        fontSize = 26.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
+    ),
+    HeadingSemiBold16 = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold
+    ),
+    SubtitleRegular16 = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
+    ),
+    BodyRegular24 = TextStyle(
+        fontSize = 24.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
+    ),
+    BodyRegular20 = TextStyle(
+        fontSize = 20.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
+    ),
+    BodySemiBold18 = TextStyle(
+        fontSize = 18.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold
+    ),
+    BodyMedium16 = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium
+    ),
+    BodyRegular16 = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
+    ),
+    BodyMedium14 = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium
+    ),
+    BodyRegular14 = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
+    ),
+    BodyRegular12 = TextStyle(
+        fontSize = 12.sp,
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal
     )
-    */
 )

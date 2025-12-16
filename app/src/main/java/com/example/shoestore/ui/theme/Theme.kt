5b9827da2fb2148b5d10e4986b5a7d5eb8c2449b
Барышев Иван <kifhok@gmail.com>
2025-12-16 12:59:24 +0300

    Изменение темы

--- app/src/main/java/com/example/shoestore/ui/theme/Theme.kt
+++ app/src/main/java/com/example/shoestore/ui/theme/Theme.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.unit.sp
 import com.example.shoestore.R
 
 
-private val сolorScheme = CustomColorScheme(
+val customColorScheme = CustomColorScheme(
     red = Red,
     accent = Accent,
     disable = Disable,
@@ -28,105 +28,22 @@ private val сolorScheme = CustomColorScheme(
 )
 
 val Inter = FontFamily(
-    Font(R.font.raleway, FontWeight.Normal),
+    Font(R.font.raleway_regular, FontWeight.Normal),
     Font(R.font.raleway_bold, FontWeight.Bold),
     Font(R.font.raleway_medium, FontWeight.Medium),
     Font(R.font.raleway_semibold, FontWeight.SemiBold)
 )
 
 val LocalCustomColors = staticCompositionLocalOf {
-    CustomColorScheme(
-        red = Red,
-        accent = Accent,
-        disable = Disable,
-        subTextLight = SubTextLight,
-        background = Background,
-        block = Block,
-        text = Text,
-        hint = Hint,
-        subTextDark = SubTextDark,
-        black = Black,
-        white = White
-    )
+   customColorScheme
 }
 val LocalCustomTypography = staticCompositionLocalOf {
-    CustomTypography(
-        HeadingRegular34 = TextStyle(
-            fontSize = 34.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        ),
-        HeadingRegular32 = TextStyle(
-            fontSize = 32.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        ),
-        HeadingBold30 = TextStyle(
-            fontSize = 30.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Bold
-        ),
-        HeadingRegular26 = TextStyle(
-            fontSize = 26.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        ),
-        HeadingSemiBold16 = TextStyle(
-            fontSize = 16.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.SemiBold
-        ),
-        SubtitleRegular16 = TextStyle(
-            fontSize = 16.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        ),
-        BodyRegular24 = TextStyle(
-            fontSize = 24.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        ),
-        BodyRegular20 = TextStyle(
-            fontSize = 20.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        ),
-        BodySemiBold18 = TextStyle(
-            fontSize = 18.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.SemiBold
-        ),
-        BodyMedium16 = TextStyle(
-            fontSize = 16.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Medium
-        ),
-        BodyRegular16 = TextStyle(
-            fontSize = 16.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        ),
-        BodyMedium14 = TextStyle(
-            fontSize = 14.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Medium
-        ),
-        BodyRegular14 = TextStyle(
-            fontSize = 14.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        ),
-        BodyRegular12 = TextStyle(
-            fontSize = 12.sp,
-            fontFamily = Inter,
-            fontWeight = FontWeight.Normal
-        )
-    )
+    customTypography
 }
 
 @Composable
 fun ShoeStoreTheme(content: @Composable () -> Unit) {
-    val customColors = сolorScheme
+    val customColors = customColorScheme
     val customTypography = customTypography
     CompositionLocalProvider(
         LocalCustomColors provides customColors,
